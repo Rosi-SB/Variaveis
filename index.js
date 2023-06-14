731566ab3b2a17694eee27adc8870c4d138673d6
Rosi-SB <122313206+Rosi-SB@users.noreply.github.com>
2023-06-14 15:19:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+alert("Página carregada com sucesso")
+
+const x=45;
+var y=10;
+
+document.write("A soma de x + y é ", x + y)
+document.write("<br>")
+document.write("A subtração de x + y é ", x - y);
+document.write("<br>");
+document.write("A multiplicação de x + y é ", x * y);
+document.write("<br>");
+document.write("A comparação de x + y é ", x == y);
